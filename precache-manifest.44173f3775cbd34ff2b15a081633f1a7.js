4f2f5d0abb17f52a459cda2e12688a9df1dff2c8
ar0ne <camtbitakou@yandex.ru>
2019-09-02 13:13:43 +0300

    Updates

---
+++ precache-manifest.44173f3775cbd34ff2b15a081633f1a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e88d46106506f7ac4831",
+    "url": "/react-minesweeper/static/css/main.c6d93b84.chunk.css"
+  },
+  {
+    "revision": "e88d46106506f7ac4831",
+    "url": "/react-minesweeper/static/js/main.e5d0fcaa.chunk.js"
+  },
+  {
+    "revision": "a36eadc15cff40b610bc",
+    "url": "/react-minesweeper/static/js/runtime~main.ea74b7a1.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/react-minesweeper/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "e9f9aa116a7f87dad62f485c7a1d6b15",
+    "url": "/react-minesweeper/index.html"
+  }
+];
